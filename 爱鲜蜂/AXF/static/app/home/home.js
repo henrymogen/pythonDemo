9c0db08f8a4b2b322f152e4de7bcabbc5ccc27f3
高明楠 <mogenhenry@gmail.com>
2018-08-04 20:49:44 +0800

    secondCommit

---
+++ 爱鲜蜂/AXF/static/app/home/home.js
@@ -0,0 +1,14 @@
+
+$(function () {
+      // 轮播
+    new Swiper('#topSwiper', {
+        loop: true,
+        // pagination:'.swiper-pagination',
+
+
+    });
+    // 必购买
+    new Swiper('#swiperMenu',{
+        slidesPerView:3,
+    })
+});
---
+++ 爱鲜蜂/AXF/static/app/order/order.js
@@ -0,0 +1,20 @@
+$(function () {
+    // 支付：需要集成第三方支付
+    // 微信支付`支付宝`银联`
+
+    //点击支付
+    $('#pay').click(function () {
+       //支付完成后，需要将订单状态更改
+        $.get('/app/orderchangestatus/', {'orderid': $(this).attr('orderid'),'status':1},function (data) {
+            // console.log(data)
+            if (data.status == 1){
+                //更改成功后进入我的页面
+                location.href = '/app/mine/'
+            }
+            else {
+                console.log(data.msg)
+            }
+        })
+
+    });
+});
